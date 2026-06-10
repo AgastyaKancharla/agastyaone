efdf46f1a1fc920d1ac50f98dbc98e112473d3f8
Agastya <Agastya.chowdary.2@gmail.com>
2026-06-11 04:08:53 +0530

    Initial AgastyaOne website

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+      <meta name="google-site-verification" content="W05c83XlGJch8hKnLcEltxSbYEgYAcSBtuE29jGqjNs" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
